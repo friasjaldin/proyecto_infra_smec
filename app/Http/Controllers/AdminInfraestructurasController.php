35d1120f6fc7aa4b6e5e6bad9a97f0bf9cd40364
Cristhian Frias Jaldin <70342601+friasjaldin@users.noreply.github.com>
2022-12-03 02:27:45 -0400

    Add files via upload

---
+++ app/Http/Controllers/AdminInfraestructurasController.php
@@ -0,0 +1,358 @@
+<?php namespace App\Http\Controllers;
+
+	use Session;
+	use Request;
+	use DB;
+	use CRUDBooster;
+
+	class AdminInfraestructurasController extends \crocodicstudio\crudbooster\controllers\CBController {
+
+	    public function cbInit() {
+
+			# START CONFIGURATION DO NOT REMOVE THIS LINE
+			$this->title_field = "id";
+			$this->limit = "5";
+			$this->orderby = "id,desc";
+			$this->global_privilege = true;
+			$this->button_table_action = true;
+			$this->button_bulk_action = false;
+			$this->button_action_style = "button_icon_text";
+			$this->button_add = true;
+			$this->button_edit = true;
+			$this->button_delete = true;
+			$this->button_detail = true;
+			$this->button_show = true;
+			$this->button_filter = true;
+			$this->button_import = false;
+			$this->button_export = true;
+			$this->table = "infraestructuras";
+			# END CONFIGURATION DO NOT REMOVE THIS LINE
+
+			# START COLUMNS DO NOT REMOVE THIS LINE
+			$this->col = [];
+			$this->col[] = ["label"=>"CODIGO SIE","name"=>"cod_sie"];
+			$this->col[] = ["label"=>"CODIGO INFRAESTRUCTURA","name"=>"cod_infraestructura"];
+			$this->col[] = ["label"=>"CODIGO SISIN","name"=>"cod_sisin"];
+			$this->col[] = ["label"=>"NOMBRE PROYECTO","name"=>"nombre_proyecto"];
+			$this->col[] = ["label"=>"DEPENDENCIA","name"=>"dependencia"];
+			$this->col[] = ["label"=>"PRESUPUESTO POA","name"=>"presupuesto_poa"];
+			$this->col[] = ["label"=>"FECHA INICIO","name"=>"fecha_inicio"];
+			$this->col[] = ["label"=>"FECHA CONCLUSION","name"=>"fecha_conclusion"];
+			$this->col[] = ["label"=>"CUCE","name"=>"cuce"];
+			$this->col[] = ["label"=>"PDF ACTA DE ENTREGA","name"=>"pdf_entrega","download"=>true];
+			$this->col[] = ["label"=>"PDF PLANOS DE CONSTRUCCION","name"=>"pdf_planos","download"=>true];
+			$this->col[] = ["label"=>"FOTOS","name"=>"fotos","download"=>true];
+			$this->col[] = ["label"=>"ACLARACIONES","name"=>"aclaraciones"];
+			# END COLUMNS DO NOT REMOVE THIS LINE
+
+			# START FORM DO NOT REMOVE THIS LINE
+			$this->form = [];
+			$this->form[] = ['label'=>'Cod Sie','name'=>'cod_sie','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Cod Infraestructura','name'=>'cod_infraestructura','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Cod Sisin','name'=>'cod_sisin','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Nombre Proyecto','name'=>'nombre_proyecto','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Dependencia','name'=>'dependencia','type'=>'select','validation'=>'required|min:1|max:255','width'=>'col-sm-10','dataenum'=>'FISCAL;CONVENIO'];
+			$this->form[] = ['label'=>'Presupuesto Poa','name'=>'presupuesto_poa','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Fecha Inicio','name'=>'fecha_inicio','type'=>'date','validation'=>'required|date','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Fecha Conclusion','name'=>'fecha_conclusion','type'=>'date','validation'=>'required|date','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Cuce','name'=>'cuce','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Pdf Entrega','name'=>'pdf_entrega','type'=>'upload','validation'=>'required|min:1|max:800000','width'=>'col-sm-10','readonly'=>'FORMATO PDF'];
+			$this->form[] = ['label'=>'Pdf Planos','name'=>'pdf_planos','type'=>'upload','validation'=>'required|min:1|max:800000','width'=>'col-sm-10','readonly'=>'FORMATO PDF'];
+			$this->form[] = ['label'=>'Fotos','name'=>'fotos','type'=>'upload','validation'=>'required|min:1|max:800000','width'=>'col-sm-10','readonly'=>'FORMATOS JPG PNG JPEG'];
+			$this->form[] = ['label'=>'Aclaraciones','name'=>'aclaraciones','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			# END FORM DO NOT REMOVE THIS LINE
+
+			# OLD START FORM
+			//$this->form = [];
+			//$this->form[] = ['label'=>'Cod Sie','name'=>'cod_sie','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Cod Infraestructura','name'=>'cod_infraestructura','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Cod Sisin','name'=>'cod_sisin','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Nombre Proyecto','name'=>'nombre_proyecto','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Dependencia','name'=>'dependencia','type'=>'select','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Presupuesto Poa','name'=>'presupuesto_poa','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Fecha Inicio','name'=>'fecha_inicio','type'=>'date','validation'=>'required|date','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Fecha Conclusion','name'=>'fecha_conclusion','type'=>'date','validation'=>'required|date','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Cuce','name'=>'cuce','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			//$this->form[] = ['label'=>'Pdf Entrega','name'=>'pdf_entrega','type'=>'upload','validation'=>'required|min:1|max:800000','width'=>'col-sm-10','readonly'=>'FORMATO PDF'];
+			//$this->form[] = ['label'=>'Pdf Planos','name'=>'pdf_planos','type'=>'upload','validation'=>'required|min:1|max:800000','width'=>'col-sm-10','readonly'=>'FORMATO PDF'];
+			//$this->form[] = ['label'=>'Fotos','name'=>'fotos','type'=>'upload','validation'=>'required|min:1|max:800000','width'=>'col-sm-10','readonly'=>'FORMATOS JPG PNG JPEG'];
+			//$this->form[] = ['label'=>'Aclaraciones','name'=>'aclaraciones','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			# OLD END FORM
+
+			/* 
+	        | ---------------------------------------------------------------------- 
+	        | Sub Module
+	        | ----------------------------------------------------------------------     
+			| @label          = Label of action 
+			| @path           = Path of sub module
+			| @foreign_key 	  = foreign key of sub table/module
+			| @button_color   = Bootstrap Class (primary,success,warning,danger)
+			| @button_icon    = Font Awesome Class  
+			| @parent_columns = Sparate with comma, e.g : name,created_at
+	        | 
+	        */
+	        $this->sub_module = array();
+
+
+	        /* 
+	        | ---------------------------------------------------------------------- 
+	        | Add More Action Button / Menu
+	        | ----------------------------------------------------------------------     
+	        | @label       = Label of action 
+	        | @url         = Target URL, you can use field alias. e.g : [id], [name], [title], etc
+	        | @icon        = Font awesome class icon. e.g : fa fa-bars
+	        | @color 	   = Default is primary. (primary, warning, succecss, info)     
+	        | @showIf 	   = If condition when action show. Use field alias. e.g : [id] == 1
+	        | 
+	        */
+	        $this->addaction = array();
+
+
+	        /* 
+	        | ---------------------------------------------------------------------- 
+	        | Add More Button Selected
+	        | ----------------------------------------------------------------------     
+	        | @label       = Label of action 
+	        | @icon 	   = Icon from fontawesome
+	        | @name 	   = Name of button 
+	        | Then about the action, you should code at actionButtonSelected method 
+	        | 
+	        */
+	        $this->button_selected = array();
+
+	                
+	        /* 
+	        | ---------------------------------------------------------------------- 
+	        | Add alert message to this module at overheader
+	        | ----------------------------------------------------------------------     
+	        | @message = Text of message 
+	        | @type    = warning,success,danger,info        
+	        | 
+	        */
+	        $this->alert        = array();
+	                
+
+	        
+	        /* 
+	        | ---------------------------------------------------------------------- 
+	        | Add more button to header button 
+	        | ----------------------------------------------------------------------     
+	        | @label = Name of button 
+	        | @url   = URL Target
+	        | @icon  = Icon from Awesome.
+	        | 
+	        */
+	        $this->index_button = array();
+
+
+
+	        /* 
+	        | ---------------------------------------------------------------------- 
+	        | Customize Table Row Color
+	        | ----------------------------------------------------------------------     
+	        | @condition = If condition. You may use field alias. E.g : [id] == 1
+	        | @color = Default is none. You can use bootstrap success,info,warning,danger,primary.        
+	        | 
+	        */
+	        $this->table_row_color = array();     	          
+
+	        
+	        /*
+	        | ---------------------------------------------------------------------- 
+	        | You may use this bellow array to add statistic at dashboard 
+	        | ---------------------------------------------------------------------- 
+	        | @label, @count, @icon, @color 
+	        |
+	        */
+	        $this->index_statistic = array();
+
+
+
+	        /*
+	        | ---------------------------------------------------------------------- 
+	        | Add javascript at body 
+	        | ---------------------------------------------------------------------- 
+	        | javascript code in the variable 
+	        | $this->script_js = "function() { ... }";
+	        |
+	        */
+	        $this->script_js = NULL;
+
+
+            /*
+	        | ---------------------------------------------------------------------- 
+	        | Include HTML Code before index table 
+	        | ---------------------------------------------------------------------- 
+	        | html code to display it before index table
+	        | $this->pre_index_html = "<p>test</p>";
+	        |
+	        */
+	        $this->pre_index_html = null;
+	        
+	        
+	        
+	        /*
+	        | ---------------------------------------------------------------------- 
+	        | Include HTML Code after index table 
+	        | ---------------------------------------------------------------------- 
+	        | html code to display it after index table
+	        | $this->post_index_html = "<p>test</p>";
+	        |
+	        */
+	        $this->post_index_html = null;
+	        
+	        
+	        
+	        /*
+	        | ---------------------------------------------------------------------- 
+	        | Include Javascript File 
+	        | ---------------------------------------------------------------------- 
+	        | URL of your javascript each array 
+	        | $this->load_js[] = asset("myfile.js");
+	        |
+	        */
+	        $this->load_js = array();
+	        
+	        
+	        
+	        /*
+	        | ---------------------------------------------------------------------- 
+	        | Add css style at body 
+	        | ---------------------------------------------------------------------- 
+	        | css code in the variable 
+	        | $this->style_css = ".style{....}";
+	        |
+	        */
+	        $this->style_css = NULL;
+	        
+	        
+	        
+	        /*
+	        | ---------------------------------------------------------------------- 
+	        | Include css File 
+	        | ---------------------------------------------------------------------- 
+	        | URL of your css each array 
+	        | $this->load_css[] = asset("myfile.css");
+	        |
+	        */
+	        $this->load_css = array();
+	        
+	        
+	    }
+
+
+	    /*
+	    | ---------------------------------------------------------------------- 
+	    | Hook for button selected
+	    | ---------------------------------------------------------------------- 
+	    | @id_selected = the id selected
+	    | @button_name = the name of button
+	    |
+	    */
+	    public function actionButtonSelected($id_selected,$button_name) {
+	        //Your code here
+	            
+	    }
+
+
+	    /*
+	    | ---------------------------------------------------------------------- 
+	    | Hook for manipulate query of index result 
+	    | ---------------------------------------------------------------------- 
+	    | @query = current sql query 
+	    |
+	    */
+	    public function hook_query_index(&$query) {
+	        //Your code here
+	            
+	    }
+
+	    /*
+	    | ---------------------------------------------------------------------- 
+	    | Hook for manipulate row of index table html 
+	    | ---------------------------------------------------------------------- 
+	    |
+	    */    
+	    public function hook_row_index($column_index,&$column_value) {	        
+	    	//Your code here
+	    }
+
+	    /*
+	    | ---------------------------------------------------------------------- 
+	    | Hook for manipulate data input before add data is execute
+	    | ---------------------------------------------------------------------- 
+	    | @arr
+	    |
+	    */
+	    public function hook_before_add(&$postdata) {        
+	        //Your code here
+
+	    }
+
+	    /* 
+	    | ---------------------------------------------------------------------- 
+	    | Hook for execute command after add public static function called 
+	    | ---------------------------------------------------------------------- 
+	    | @id = last insert id
+	    | 
+	    */
+	    public function hook_after_add($id) {        
+	        //Your code here
+
+	    }
+
+	    /* 
+	    | ---------------------------------------------------------------------- 
+	    | Hook for manipulate data input before update data is execute
+	    | ---------------------------------------------------------------------- 
+	    | @postdata = input post data 
+	    | @id       = current id 
+	    | 
+	    */
+	    public function hook_before_edit(&$postdata,$id) {        
+	        //Your code here
+
+	    }
+
+	    /* 
+	    | ---------------------------------------------------------------------- 
+	    | Hook for execute command after edit public static function called
+	    | ----------------------------------------------------------------------     
+	    | @id       = current id 
+	    | 
+	    */
+	    public function hook_after_edit($id) {
+	        //Your code here 
+
+	    }
+
+	    /* 
+	    | ---------------------------------------------------------------------- 
+	    | Hook for execute command before delete public static function called
+	    | ----------------------------------------------------------------------     
+	    | @id       = current id 
+	    | 
+	    */
+	    public function hook_before_delete($id) {
+	        //Your code here
+
+	    }
+
+	    /* 
+	    | ---------------------------------------------------------------------- 
+	    | Hook for execute command after delete public static function called
+	    | ----------------------------------------------------------------------     
+	    | @id       = current id 
+	    | 
+	    */
+	    public function hook_after_delete($id) {
+	        //Your code here
+
+	    }
+
+
+
+	    //By the way, you can still create your own method in here... :) 
+
+
+	}
